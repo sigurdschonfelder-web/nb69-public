dc398bbd0bf79df441e1addaf93a5e4f4ea03fff
Sigurd Schønfelder <sigurdws@stud.ntnu.no>
2025-10-17 14:42:32 +0200

    Initial commit: Spring Boot + React structure

---
+++ backend/NB69/src/main/java/sigurdws/NB69/Nb69Application.java
@@ -0,0 +1,13 @@
+package sigurdws.NB69;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Nb69Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Nb69Application.class, args);
+	}
+
+}
---
+++ backend/NB69/src/test/java/sigurdws/NB69/Nb69ApplicationTests.java
@@ -0,0 +1,13 @@
+package sigurdws.NB69;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Nb69ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
