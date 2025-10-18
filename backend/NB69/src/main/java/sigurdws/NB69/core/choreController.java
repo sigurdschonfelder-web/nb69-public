c62a133101a043a6381b47d8b8d733a6c2676afd
Sigurd Schønfelder <sigurdws@stud.ntnu.no>
2025-10-18 20:18:45 +0200

    fix

--- backend/NB69/src/main/java/sigurdws/NB69/core/choreController.java
+++ backend/NB69/src/main/java/sigurdws/NB69/core/choreController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/chores")
 @CrossOrigin(origins = {
         "http://localhost:5173",
-        "https://<DITT-FRONTEND-DOMENE>.vercel.app" // Bytt til domene
+        "https://nb69-frontend.vercel.app" // Bytt til domene
 })
 public class choreController {
 
