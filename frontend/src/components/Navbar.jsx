5929f88695f0d0957c5c1437a47c87490088b4ee
Sigurd Schønfelder <sigurdws@stud.ntnu.no>
2025-10-19 23:00:56 +0200

    fix typo

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import { Link, NavLink } from "react-router-dom";
-import "./navbar.css";
+import "./Navbar.css";
 
 export default function Navbar() {
   const [open, setOpen] = useState(false);
