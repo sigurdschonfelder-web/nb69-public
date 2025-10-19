8582f3b97c2716f5c87d47c96e267084294fca41
Sigurd Schønfelder <sigurdws@stud.ntnu.no>
2025-10-19 15:57:55 +0200

    fix: typo

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,7 +5,7 @@ import ChoreTest from "./components/ChoreTest";
 export default function App() {
   return (
     <div style={{ padding: 16, fontFamily: "sans-serif" }}>
-      <h1>Ansvarsområdet</h1>
+      <h1>Ansvarsområder</h1>
       <ChoreTest />
     </div>
   );
