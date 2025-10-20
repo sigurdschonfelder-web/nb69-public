212dae5618af9bc93b761afa8ffdbdaa46bad231
Sigurd Schønfelder <sigurdws@stud.ntnu.no>
2025-10-20 19:09:07 +0200

    fix: prikk reset + sorted

--- backend/NB69/src/main/java/sigurdws/NB69/prikker/PrikkerController.java
+++ backend/NB69/src/main/java/sigurdws/NB69/prikker/PrikkerController.java
@@ -4,6 +4,8 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
 
 @RestController
@@ -16,11 +18,12 @@ public class PrikkerController {
     @GetMapping("/api/prikker")
     public List<PrikkRow> getPrikker() {
         // Midlertidige data – byttes senere til database/fil om ønskelig
-        return List.of(
-                new PrikkRow("Andreas", 2),
-                new PrikkRow("Jørgen", 5),
-                new PrikkRow("Erlend", 1),
-                new PrikkRow("Eilif", 3),
-                new PrikkRow("Sigurd", 0));
+        List<PrikkRow> prikker = new ArrayList<>(List.of(
+                new PrikkRow("Andreas", 0),
+                new PrikkRow("Jørgen", 0),
+                new PrikkRow("Erlend", 0),
+                new PrikkRow("Eilif", 0),
+                new PrikkRow("Sigurd", 0)));
+        return prikker.stream().sorted((o1, o2) -> o1.prikker() - o2.prikker()).toList();
     }
 }
